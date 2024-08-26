2941d2e3ff567bcf3aee0f9e69f9c7c771da5178
Joel Gonçalves <jl.goncalves@outlook.com.br>
2024-08-26 17:58:26 -0300

    feat: refactor enviroment docker-compose

---
+++ docker.sh
@@ -0,0 +1,15 @@
+docker-compose up -d
+docker exec -it localmongo1 mongo
+
+rs.initiate(
+  {
+    _id : 'rs0',
+    members: [
+      { _id : 0, host : "mongo1:27017" },
+      { _id : 1, host : "mongo2:27017" },
+      { _id : 2, host : "mongo3:27017", arbiterOnly: true }
+    ]
+  }
+)
+
+exit
