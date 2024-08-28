6ad839d4cd4cd24443698fe1055ee45086546fd3
Joel Gonçalves <jl.goncalves@outlook.com.br>
2024-08-28 11:17:37 -0300

    feat: create new provider to generate proof pdf

---
+++ src/providers/generate-prof.provider.ts
@@ -0,0 +1,113 @@
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+
+interface IProofPayment {
+    name: string;
+    totalValue: number;
+    checkIn: string;
+    checkOut: string;
+    days: number;
+    dailyValue: number;
+}
+export class GenerateProofProvider {
+
+    async execute({ name, totalValue, checkIn, checkOut, days, dailyValue }: IProofPayment): Promise<Uint8Array> {
+        const pdfDoc = await PDFDocument.create();
+        const page = pdfDoc.addPage([600, 400]);
+
+        const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+        const { width, height } = page.getSize();
+
+        const titleFontSize = 18;
+        const textFontSize = 12;
+
+        page.drawText('Comprovante de Pagamento', {
+            x: 50,
+            y: height - 50,
+            size: titleFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Nome do Cliente: ${name}`, {
+            x: 50,
+            y: height - 80,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Valor Total: R$ ${totalValue}`, {
+            x: 50,
+            y: height - 100,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText('Período da Estadia:', {
+            x: 50,
+            y: height - 120,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Check-in: ${checkIn}`, {
+            x: 70,
+            y: height - 140,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Check-out: ${checkOut}`, {
+            x: 70,
+            y: height - 160,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Número de Dias: ${days}`, {
+            x: 50,
+            y: height - 180,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText('Detalhamento do Pagamento:', {
+            x: 50,
+            y: height - 200,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Valor da Diária: R$ ${dailyValue} por noite`, {
+            x: 70,
+            y: height - 220,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText(`Valor Total da Estadia: R$ ${totalValue} (Calculado como ${days} noites x R$ ${dailyValue} por noite)`, {
+            x: 50,
+            y: height - 240,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        page.drawText('Obrigado por escolher nossos serviços. Desejamos a você uma excelente estadia!', {
+            x: 50,
+            y: height - 270,
+            size: textFontSize,
+            font: font,
+            color: rgb(0, 0, 0),
+        });
+
+        return pdfDoc.save();
+    }
+}
--- src/services/proof-payment.service.ts
+++ src/services/proof-payment.service.ts
@@ -1,6 +1,5 @@
 import { Logger } from "pino";
 import { PreconditionFailed } from 'http-errors'
-import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 import * as dayjs from 'dayjs';
 
 import { CustomerRepository } from "../repositories/customer.repository";
@@ -10,127 +9,40 @@ import { ProofPaymentDto } from "../dtos/proof-payment.dto";
 import { PROOF_PAYMENT_BUCKET } from "../common/constant/cloud-storage.constant";
 
 import 'dayjs/locale/pt-br';
+import { GenerateProofProvider } from "../providers/generate-prof.provider";
 dayjs.locale('pt-br');
 
 export class ProofPaymentService {
     private readonly logger: Logger;
     private readonly customerRepository: CustomerRepository;
     private readonly saveFileProvider: SaveFileProvider;
+    private readonly generateProofProvider: GenerateProofProvider;
 
     constructor() {
         this.logger = logger;
         this.customerRepository = new CustomerRepository();
         this.saveFileProvider = new SaveFileProvider();
+        this.generateProofProvider = new GenerateProofProvider();
     }
 
     async execute(proofPaymentMessaging: ProofPaymentDto) {
         const customer = await this.customerRepository.findById(proofPaymentMessaging.customerId);
         if (!customer) throw new PreconditionFailed();
 
-        const pdfDoc = await PDFDocument.create();
-        const page = pdfDoc.addPage([600, 400]);
-
-        const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-        const { width, height } = page.getSize();
-
-        const titleFontSize = 18;
-        const textFontSize = 12;
-
-        page.drawText('Comprovante de Pagamento', {
-            x: 50,
-            y: height - 50,
-            size: titleFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
+        const pdfBytes = await this.generateProofProvider.execute({
+            name: customer.name,
+            totalValue: proofPaymentMessaging.totalValue,
+            days: proofPaymentMessaging.days,
+            dailyValue: proofPaymentMessaging.dailyValue,
+            checkIn: dayjs(proofPaymentMessaging.checkIn).format('D [de] MMMM [de] YYYY'),
+            checkOut: dayjs(proofPaymentMessaging.checkOut).format('D [de] MMMM [de] YYYY'),
         });
-
-        page.drawText(`Nome do Cliente: ${customer.name}`, {
-            x: 50,
-            y: height - 80,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Valor Total: R$ ${proofPaymentMessaging.totalValue}`, {
-            x: 50,
-            y: height - 100,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText('Período da Estadia:', {
-            x: 50,
-            y: height - 120,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Check-in: ${proofPaymentMessaging.checkIn}`, {
-            x: 70,
-            y: height - 140,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Check-out: ${proofPaymentMessaging.checkOut}`, {
-            x: 70,
-            y: height - 160,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Número de Dias: ${proofPaymentMessaging.days}`, {
-            x: 50,
-            y: height - 180,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText('Detalhamento do Pagamento:', {
-            x: 50,
-            y: height - 200,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Valor da Diária: R$ ${proofPaymentMessaging.dailyValue} por noite`, {
-            x: 70,
-            y: height - 220,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText(`Valor Total da Estadia: R$ ${proofPaymentMessaging.totalValue} (Calculado como ${proofPaymentMessaging.days} noites x R$ ${proofPaymentMessaging.dailyValue} por noite)`, {
-            x: 50,
-            y: height - 240,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        page.drawText('Obrigado por escolher nossos serviços. Desejamos a você uma excelente estadia!', {
-            x: 50,
-            y: height - 270,
-            size: textFontSize,
-            font: font,
-            color: rgb(0, 0, 0),
-        });
-
-        const pdfBytes = await pdfDoc.save();
 
         const isSuccess = await this.saveFileProvider.execute({ 
             file: pdfBytes,
             bucket: PROOF_PAYMENT_BUCKET,
             contentType: 'application/pdf',
-            key: `${proofPaymentMessaging.customerId}/${proofPaymentMessaging.reservationId}`
+            key: `${proofPaymentMessaging.customerId}/${proofPaymentMessaging.reservationId}.pdf`
         });
 
         if (!isSuccess) throw new PreconditionFailed();
