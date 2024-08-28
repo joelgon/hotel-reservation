9a383cb5f0253831f2c96d2dfd4fe723c2a54b51
Joel Gonçalves <jl.goncalves@outlook.com.br>
2024-08-28 14:16:38 -0300

    feat: config lint, prettier and editorConfig

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginImport from "eslint-plugin-import";
+
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index"
+          ],
+          "newlines-between": "always",
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          }
+        }
+      ]
+    }
+  },
+  eslintConfigPrettier
+];
